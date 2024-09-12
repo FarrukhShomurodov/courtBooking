e5e516382be8a697ace23ff6dfe9e616e647a9c6
FarrukhShomurodov <uamerike@gmail.com>
2024-09-12 05:23:25 +0500

    scale disable

--- resources/views/findz/layouts/app.blade.php
+++ resources/views/findz/layouts/app.blade.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
     <title>Findz</title>
     <link rel="stylesheet" href="{{ asset('css/findz/style.css') }}"/>
     <link rel="stylesheet" href="{{ asset('css/findz/findz.css') }}"/>
