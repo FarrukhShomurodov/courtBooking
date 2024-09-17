4e23bde0c53685a364f1a4d838508cb0cd119c80
FarrukhShomurodov <uamerike@gmail.com>
2024-09-17 17:12:37 +0500

    test

--- resources/views/findz/pages/stadiums.blade.php
+++ resources/views/findz/pages/stadiums.blade.php
@@ -14,9 +14,6 @@
 @section('header')
     <header class="d-flex row justify-content-center align-items-center">
         <h3 class="findz">FINDZ</h3>
-        {{--        <a href="{{ route('findz.filter', $currentSportTypeId) }}">--}}
-        {{--            <img src="{{ asset('img/findz/icons/filter.svg') }}" alt="filter icon">--}}
-        {{--        </a>--}}
     </header>
 @endsection
 
@@ -32,57 +29,6 @@
             </ul>
         </div>
     </nav>
-
-    <div class="select-lang">
-        @if (app()->getLocale() == 'uz')
-            <a href="{{ url('set-lang/uz') }}">
-                <button class="selected-lang">
-                    Uz
-                </button>
-            </a>
-            <a href="{{ url('set-lang/ru') }}">
-                <button>
-                    Ru
-                </button>
-            </a>
-        @else
-            <a href="{{ url('set-lang/ru') }}">
-                <button class="selected-lang">
-                    Ru
-                </button>
-            </a>
-            <a href="{{ url('set-lang/uz') }}">
-                <button>
-                    Uz
-                </button>
-            </a>
-        @endif
-    </div>
-
-
-    @if(request('date') || request('start_time') || request('end_time'))
-        <div class="date_time d-flex row align-items-center gap-12">
-            @if(request('date'))
-                <div class="date d-flex row align-items-center justify-content-center">
-                    <p>{{ request('date') }}</p>
-                    <a href="{{ route('findz.stadiums.filter.sport.type', array_merge(request()->query(), ['sportType' => $currentSportTypeId, 'date' => null, 'start_time' => request('start_time'), 'end_time' => request('end_time')])) }}"
-                       class="pointer ml-10">
-                        <img src="{{ asset('img/findz/icons/cancel.svg') }}" alt="cancel icon">
-                    </a>
-                </div>
-            @endif
-            @if(request('start_time') && request('end_time'))
-                <div class="time d-flex row align-items-center justify-content-center">
-                    <p>{{ request('start_time') }} - {{ request('end_time') }}</p>
-                    <a href="{{ route('findz.stadiums.filter.sport.type', array_merge(request()->query(), ['sportType' => $currentSportTypeId, 'date' => request('date'), 'start_time' => null, 'end_time' => null])) }}"
-                       class="pointer ml-10">
-                        <img src="{{ asset('img/findz/icons/cancel.svg') }}" alt="cancel icon">
-                    </a>
-                </div>
-            @endif
-        </div>
-    @endif
-
 
     <div class="container_mobile">
         <div class="content">
@@ -128,28 +74,6 @@
                 }
             });
 
-            $('#lang-icon').on('click', function() {
-                $('.select-lang').css('display', 'block');
-                $('.container_mobile').css('margin', '152px auto 0', 'important');
-
-                @if(request('date') || request('start_time') || request('end_time'))
-                $('.date_time ').css('top', '136px');
-                $('.container_mobile').css('margin', '202px  auto 0', 'important');
-                @endif
-            });
-
-            $(window).on('click', function(event) {
-                if (!$(event.target).closest('.select-lang, #lang-icon').length) {
-                    $('.select-lang').hide();
-                    // $('.container_mobile').css('margin', '122px auto 0', 'important');
-
-                    @if(request('date') || request('start_time') || request('end_time'))
-                    $('.date_time ').css('top', '95px');
-                    // $('.container_mobile').css('margin', '182px auto 0', 'important');
-                    @endif
-                }
-            });
-
             $('.scroll-wrapper').on('wheel', function (event) {
                 if (event.originalEvent.deltaY !== 0) {
                     this.scrollLeft += event.originalEvent.deltaY;
@@ -157,27 +81,6 @@
                 }
             });
 
-            $('#lang-icon').on('click', function () {
-                $('.select-lang').css('display', 'block');
-                $('.container_mobile').css('margin', '152px auto 0', 'important');
-
-                @if(request('date') || request('start_time') || request('end_time'))
-                $('.date_time ').css('top', '136px');
-                // $('.container_mobile').css('margin', '202px  auto 0', 'important');
-                @endif
-            });
-
-            $(window).on('click', function (event) {
-                if (!$(event.target).closest('.select-lang, #lang-icon').length) {
-                    $('.select-lang').hide();
-                    $('.container_mobile').css('margin', '122px auto 0', 'important');
-
-                    @if(request('date') || request('start_time') || request('end_time'))
-                    $('.date_time ').css('top', '95px');
-                    $('.container_mobile').css('margin', '182px auto 0', 'important');
-                    @endif
-                }
-            });
         });
     </script>
 @endsection
