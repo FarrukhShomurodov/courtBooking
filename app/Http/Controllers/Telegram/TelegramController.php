5effe7d33ee6c7536b2c6da2d8fe48c65c7856cd
FarrukhShomurodov <uamerike@gmail.com>
2024-09-21 11:53:09 +0500

    local book

--- app/Http/Controllers/Telegram/TelegramController.php
+++ app/Http/Controllers/Telegram/TelegramController.php
@@ -548,10 +548,10 @@ class TelegramController extends Controller
 
             $description = "*Стадион:* {$booking->court->stadium->name}\n"
 //                . "*".__('court.court').":* {$booking->court->name}\n"
-                .__('findz/book.address').":* {$booking->court->stadium->address}\n"
-                .__('findz/book.date').":* {$booking->date}\n"
-                .__('findz/book.time').":* {$booking->start_time} - {$booking->end_time}\n"
-                .__('findz/book.price').":* *" . round($booking->price / 1000) . " " . __('findz/book.currency') . "*\n\n";
+                .__('findz/book.address')." {$booking->court->stadium->address}\n"
+                .__('findz/book.date')." {$booking->date}\n"
+                .__('findz/book.time')." {$booking->start_time} - {$booking->end_time}\n"
+                .__('findz/book.price'). round($booking->price / 1000) . " " . __('findz/book.currency') . "*\n\n";
 
             if ($hoursRemaining <= 24) {
                 $description .= __('findz/book.edit_book_info')."_\n";
@@ -567,7 +567,7 @@ class TelegramController extends Controller
                         'type' => 'photo',
                         'media' => $fullPhotoUrl,
                         'caption' => $index === 0 ? $description : '',
-//                        'parse_mode' => 'Markdown'
+                        'parse_mode' => 'Markdown'
                     ]);
                 }
 
